8216b736fa88d07e4f1a66080253d2148768ff2b
Keegan White <keeganthomaswhite@gmail.com>
2024-06-19 15:56:43 +0200

    started project

---
+++ index.js
@@ -0,0 +1,28 @@
+import Web3 from "web3";
+import { newKitFromWeb3, CeloContract } from "@celo/contractkit";
+
+const web3 = new Web3("https://alfajores-forno.celo-testnet.org");
+const kit = newKitFromWeb3(web3);
+
+// Main async function to run your application logic
+async function main() {
+    console.log("Starting the application...");
+
+    let accounts = await kit.web3.eth.getAccounts();
+    kit.defaultAccount = accounts[0];
+    // paid gas in cUSD
+    await kit.setFeeCurrency(CeloContract.StableToken);
+
+    let totalBalance = await kit.getTotalBalance(myAddress);
+    console.log("Operation completed successfully.");
+}
+
+
+(async () => {
+    try {
+        await main();
+        console.log('Finished execution');
+    } catch (error) {
+        console.error('Error running the main function', error);
+    }
+})();
