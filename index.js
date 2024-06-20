3a590c9b7d51363c39881b1607206af9c75dad1c
Keegan White <keeganthomaswhite@gmail.com>
2024-06-20 09:28:34 +0200

    server side code working

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+// Imports
 import Web3 from "web3";
-import { newKitFromWeb3, CeloContract } from "@celo/contractkit";
+import { newKitFromWeb3 } from "@celo/contractkit";
 import 'dotenv/config';
+import * as fs from "node:fs";
+
+// Variables
 const web3 = new Web3("https://forno.celo.org");
 const kit = newKitFromWeb3(web3);
-import * as fs from "node:fs";
 const secret_path = process.env.SECRET_PATH;
-
 const contractAddress = process.env.CONTRACT_ADDRESS;
 if (!web3.utils.isAddress(contractAddress)) {
     throw new Error("Invalid Ethereum address from environment variables");
 }
-
 const myAddress = process.env.MY_ADDRESS;
 if (!web3.utils.isAddress(myAddress)) {
     throw new Error("Invalid Ethereum address from environment variables");
 }
+
+
 
 function createAccount_contract_kit() {
     console.log('Creating a new account')
@@ -47,9 +50,6 @@ async function main() {
 
     let totalBalance = await kit.getTotalBalance(myAddress);
     console.log(`Preset account has a total balance for ${myAddress}:`, totalBalance);
-
-    console.log("Celo interactions with thirdweb SDK");
-
 
 }
 
